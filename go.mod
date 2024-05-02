d64b4beedfcde7dafbdff59aa7f64fe3202ed3eb
Emerson <emerson.day@gmail.com>
2024-05-01 19:52:49 -0700

    bootdev cli

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/emday4prez/tweeter
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	mux := http.NewServeMux()
+
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
