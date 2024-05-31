2bb1619cdca6b8d01301b21d2105f305f81a2344
Emerson <emerson.day@gmail.com>
2024-05-30 20:55:36 -0700

    users post

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ flag.Parse()
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 	mux.HandleFunc("GET /api/chirps/{id}", apiCfg.handlerChirpsGetById)
 
